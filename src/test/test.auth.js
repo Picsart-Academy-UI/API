7cc65558b3439e604b97dfafa8e04258b3534554
Armen Nersisyan <armennersisyan@Armens-MacBook-Pro-2.local>
2020-12-19 21:49:51 +0400

    initializing mocha and chai

---
+++ src/test/test.auth.js
@@ -0,0 +1,10 @@
+const { expect } = require('chai');
+const { describe, it } = require('mocha');
+
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should test', () => {
+      expect(true).to.equal(true);
+    });
+  });
+});
