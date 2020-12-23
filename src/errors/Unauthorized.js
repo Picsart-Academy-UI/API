2d1ccf1f545256bef1427f7d794f015fef36397a
adamyanlina <adamyanlina97@gmail.com>
2020-12-24 01:13:02 +0400

    Changed Unauthorized constructor

--- src/errors/Unauthorized.js
+++ src/errors/Unauthorized.js
@@ -1,8 +1,8 @@
 const { HttpError } = require('./HttpError');
 
 export class Unauthorized extends HttpError {
-  constructor() {
-    super();
+  constructor(user) {
+    super(`${user} was not authorized.`);
     this.name = this.constructor.name;
     this.statusCode = 401;
     HttpError.captureStackTrace(this, this.constructor);
