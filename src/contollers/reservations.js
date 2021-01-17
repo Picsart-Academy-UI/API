eb2bbf06be497b501b163f539167eddd1fdfe235
Vardges <vardges.mushegyan@gmail.com>
2021-01-17 21:46:39 +0400

    check reservations

--- src/contollers/reservations.js
+++ src/contollers/reservations.js
@@ -14,11 +14,19 @@ const {
   updateReservation,
   createReservation} = require('../utils/reservation-helpers');
 
+// @desc  create reservation
+// @route POST => /api/v1/reservations
+// @access Private (User/Admin)
+
 exports.create = asyncHandler(async (req, res) => {
   const reservation = await createReservation(req);
   return res.status(201)
     .json({data: reservation});
 });
+
+// @desc  update reservation
+// @route PUT => /api/v1/reservations/:reservation_id
+// @access Private (User/Admin)
 
 exports.update = asyncHandler(async (req, res) => {
   const reservation = await updateReservation(req);
@@ -27,6 +35,11 @@ exports.update = asyncHandler(async (req, res) => {
   });
 
 });
+
+
+// @desc  get reservations
+// @route GET => /api/v1/reservations
+// @access Private (Admin)
 
 exports.getAll = asyncHandler(async (req, res) => {
 
