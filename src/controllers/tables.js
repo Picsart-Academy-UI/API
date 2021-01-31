95142aca6d243cfefe77680c1b9cecb2ca9f8feb
adamyanlina <adamyanlina97@gmail.com>
2021-02-01 02:54:31 +0400

    Delete chair conntroller&routes

--- src/controllers/tables.js
+++ src/controllers/tables.js
@@ -1,9 +1,10 @@
 const { Table } = require('booking-db');
 
 const { NotFound } = require('../utils/errorResponse');
-const { createChairs } = require('../utils/createChairs');
 const { buildQuery, getPagination } = require('../utils/util');
 const { asyncHandler } = require('../middlewares/asyncHandler');
+const { createChairs, deleteAllChairsInTable } = require('../utils/createChairs');
+
 
 exports.create = asyncHandler(async (req, res, next) => {
   const table = await Table.create(req.body);
@@ -17,7 +18,7 @@ exports.getAll = asyncHandler(async (req, res, next) => {
 
   const tables = await Table
     .find()
-    .populate({ path: 'chairs', select: '_id number -table_id' })
+    .populate({ path: 'chairs', select: '_id chair_number -table_id' })
     .lean()
     .exec();
 
@@ -42,6 +43,7 @@ exports.getOne = asyncHandler(async (req, res, next) => {
 
   if (!table) next(new NotFound());
 
+
   return res.status(200).json({ data: table });
 });
 
@@ -53,6 +55,9 @@ exports.update = asyncHandler(async (req, res, next) => {
   );
 
   if (!table) next(new NotFound());
+
+  await deleteAllChairsInTable(table);
+  await createChairs(table);
 
   return res.status(200).json({ data: table });
 });
